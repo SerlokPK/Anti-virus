08d56fcef16427a17ff5db6ef01ad4c52811845b
Dalibor Oletic <doletic1@gmail.com>
2018-11-05 15:51:37 +0100

    models, contracts libraries added

---
+++ MDMS.IntrusionDetectionSystem/IntrusionService.cs
@@ -0,0 +1,27 @@
+﻿using MDMS.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDMS.IntrusionDetectionSystem
+{
+    public class IntrusionService : IIntrusionService
+    {
+        public IntrusionService()
+        {
+        }
+
+        private void LoadXMLFile()
+        {
+
+        }
+
+        public void AddIntrusion()
+        {
+
+        }
+
+    }
+}
