32494102f6662ec7c61ceb1a3d17f49f200c7d3a
DaliborOletic <doletic1@gmail.com>
2018-11-05 23:24:38 +0100

    Logger added

--- MDMS.Common/Utilities/IEnumerableExtensions.cs
+++ MDMS.Common/Utilities/IEnumerableExtensions.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using log4net;
+using log4net.Config;
 using MDMS.Utilities.Models;
 
 namespace MDMS.Utilities.Utilities
 {
     public static class IEnumerableExtensions
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         static IEnumerableExtensions()
         {
-            
         }
 
         public static IEnumerable<Tuple<string, string, int>> LogIntrusions(this IEnumerable<Tuple<string, string, int>> data)
@@ -24,7 +25,7 @@ namespace MDMS.Utilities.Utilities
 
             foreach(var tuple in data)
             {
-                log.Info($"Critical: Name:{tuple.Item1}, User:{tuple.Item2}, Count:{tuple.Item3}");
+                Logger.Info($"Critical: Name:{tuple.Item1}, User:{tuple.Item2}, Count:{tuple.Item3}");
             }
 
             return data;
