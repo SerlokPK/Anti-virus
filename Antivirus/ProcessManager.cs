ee4d9a095b7df8d19b49c2d8cdf2319cfa2b121e
serbulas1 <serbulas1@gmail.com>
2018-11-06 15:55:32 +0100

    Compare all processes with unauthorized one - done

---
+++ Antivirus/ProcessManager.cs
@@ -0,0 +1,56 @@
+﻿using MDMS.Utilities.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antivirus
+{
+    public class ProcessManager
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+        /// <summary>
+        /// Checks if there is active unauthorized process in list of processes
+        /// </summary>
+        /// <returns></returns>
+        public static List<ProcessModel> CheckIfUnauthorizedExists(List<ProcessModel> blackList)
+        {
+            var processes = Process.GetProcesses().ToList();
+
+            return processes.SelectMany(x => blackList.FindAll(y => y.Name == x.ProcessName)).ToList();
+            //return processes.Select(s1 => s1.ProcessName ).ToList().Intersect(blackList.Select(s2 => s2.Name).ToList()).ToList();
+
+        }
+
+        private static string GetProcessUser(Process process)
+        {
+            IntPtr processHandle = IntPtr.Zero;
+            try
+            {
+                OpenProcessToken(process.Handle, 8, out processHandle);
+                WindowsIdentity wi = new WindowsIdentity(processHandle);
+                string user = wi.Name;
+                return user.Contains(@"\") ? user.Substring(user.IndexOf(@"\") + 1) : user;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (processHandle != IntPtr.Zero)
+                {
+                    CloseHandle(processHandle);
+                }
+            }
+        }
+    }
+}
--- Antivirus/Program.cs
+++ Antivirus/Program.cs
@@ -3,8 +3,11 @@ using MDMS.Utilities;
 using MDMS.Utilities.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Antivirus
@@ -13,9 +16,27 @@ namespace Antivirus
     {
         static void Main(string[] args)
         {
-            List<ProcessModel> blackList = ConvertJson.Deserialize($"..\\..\\BlacklistConfig.json");
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9519/IntrusionService";
+            Task invoker = Task.Factory.StartNew(Checker);
 
             Console.ReadKey();
+        }
+
+        private static void Checker()
+        {
+            List<ProcessModel> blackList = ConvertJson.Deserialize($"..\\..\\BlacklistConfig.json");
+            List<ProcessModel> namesOfProcesses = ProcessManager.CheckIfUnauthorizedExists(blackList);
+
+            while (true)
+            {
+                if (namesOfProcesses.Count > 0)
+                {
+                   // var foundedProcesses = blackList.FindAll(x => namesOfProcesses.Contains(x.Name));
+                }
+
+                Thread.Sleep(15000);
+            }
         }
     }
 }
