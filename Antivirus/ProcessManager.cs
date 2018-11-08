5d218fecb4607de3c29023af5c9d87dad4141b2d
serbulas1 <serbulas1@gmail.com>
2018-11-08 18:18:34 +0100

    Checking user of every process in windows

--- Antivirus/ProcessManager.cs
+++ Antivirus/ProcessManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Text;
@@ -12,11 +13,6 @@ namespace Antivirus
 {
     public class ProcessManager
     {
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseHandle(IntPtr hObject);
         /// <summary>
         /// Checks if there is active unauthorized process in list of processes
         /// </summary>
@@ -26,33 +22,30 @@ namespace Antivirus
             var processes = Process.GetProcesses().ToList();
                                                                                                                             //if we have greater hour, we don't need to check minutes, otherwise, we need to check minutes,
                                                                                                                             //it's all done because of zero
-            var unauthorized = processes.SelectMany(x => blackList.FindAll(y => y.Name == x.ProcessName && y.User == GetProcessUser(x) && ((y.StartHours < DateTime.Now.Hour) || (y.StartHours == DateTime.Now.Hour && y.StartMinutes <= DateTime.Now.Minute))
+            var unauthorized = processes.SelectMany(x => blackList.FindAll(y => y.Name == x.ProcessName && y.User == GetProcessOwner(x.Id) && ((y.StartHours < DateTime.Now.Hour) || (y.StartHours == DateTime.Now.Hour && y.StartMinutes <= DateTime.Now.Minute))
                                                                                                                 && ((y.EndHours > DateTime.Now.Hour) || (y.EndHours == DateTime.Now.Hour && y.EndMinutes >= DateTime.Now.Minute)))).ToList();
 
             return unauthorized;
         }
 
-        private static string GetProcessUser(Process process)
+
+        public static string GetProcessOwner(int processId)
         {
-            IntPtr processHandle = IntPtr.Zero;
-            try
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectCollection calledProcess = new ManagementObjectSearcher(query).Get();
+
+            foreach (ManagementObject obj in calledProcess)
             {
-                OpenProcessToken(process.Handle, 8, out processHandle);
-                WindowsIdentity wi = new WindowsIdentity(processHandle);
-                string user = wi.Name;
-                return user.Contains(@"\") ? user.Substring(user.IndexOf(@"\") + 1) : user;
-            }
-            catch
-            {
-                return null;
-            }
-            finally
-            {
-                if (processHandle != IntPtr.Zero)
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
                 {
-                    CloseHandle(processHandle);
+                    // return DOMAIN\user
+                    return argList[0];
                 }
             }
+
+            return "NO OWNER";
         }
     }
 }
